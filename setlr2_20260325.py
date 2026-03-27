4710f7951e106d55674fc9e8ea19079348d95e98
kimminsu147 <kimminsu1059@gmail.com>
2026-03-27 12:26:30 +0900

    Update setlr2_20260325.py

--- setlr2_20260325.py
+++ setlr2_20260325.py
@@ -1,5 +1,12 @@
 from ultralytics import RTDETR
 
+#training rtdetr code
+#epoch is too many=> you should set amp=False(in this case, epochs=200, so I set amp=False)
+#lr0 is too big=> loss doesn't go down well, so I set lr0=0.0001
+#to make code working well, I set warmup_bias_lr=0.0001
+#optimizer=> if you don't set this, then this set to 'auto'. => your set lr0 does't apply => so I set this 'AdamW'
+#momentum=0.9, lrf=0.01, cos_lr=False is just setting value that if you don't write it.
+
 model_rtdetr=RTDETR('rtdetr-l.pt')
 DATAPATH='/home/tuanluong/coop2026_1/ParkingLot/data.yaml'
 resutls_rtdetr=model_rtdetr(data=DATAPATH, epochs=200, imgsz=640, batch=8, cos_lr=False, optimizer='AdamW', momentum=0.9, amp=False, lr0=0.0001, lrf=0.01, warmup_epochs=3, warmup_bias_lr=0.0001, name='rtdetr_epoch200_setlr2')
