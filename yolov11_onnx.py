2598398b6bad4019edeebe695aea25aa567f0ba5
kimminsu147 <kimminsu1059@gmail.com>
2026-03-30 00:45:54 +0900

    Add files via upload
    This .py files and .onnx file are updated files for YOLO version 11. I fixed 'yolov8_onnx.py' and related python files which were made for detection by using YOLO version 8.

---
+++ yolov11_onnx.py
@@ -0,0 +1,30 @@
+import sys
+
+import numpy as np
+
+import onnxruntime as ort
+
+from utils_v11 import preprocess_yolo, YoloPostProcess
+from visualize_v11 import YoloVisualizer
+
+YOLO_ONNX_PATH = '/home/rise/Sim/yolov11/modified_best_yolo200.onnx'
+DATASET = [
+    '/home/rise/Sim/yolov11/public02.jpg',
+]
+
+def main(args):
+    model = ort.InferenceSession(YOLO_ONNX_PATH, providers = ['CUDAExecutionProvider', 'CPUExecutionProvider'])
+    postprocess = YoloPostProcess()
+    visualizer = YoloVisualizer()
+    
+    for file_path in DATASET:
+        file_name = file_path.split("/")[-1]
+        img = preprocess_yolo(file_path)
+        img = np.expand_dims(np.transpose(img, [2, 0, 1]), 0)
+        outputs = model.run(None, {'images': img})
+        result = postprocess.run(outputs)
+        visualizer.save(result, input_path=file_path, output_path=f"output/{file_name}")
+        
+
+if __name__ == '__main__':
+    main(sys.argv)
